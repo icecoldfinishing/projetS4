59a4ce0ad72df6a44121cbc2e0a7eb5e5513cf2e
icecoldfinishing <rohyamboara@gmail.com>
2025-07-07 14:33:33 +0300

    sqxsxqs

---
+++ ws/routes/routes.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . '/../controllers/EtudiantController.php';
+require_once __DIR__ . '/../controllers/other/HelloController.php';
+require_once __DIR__ . '/../controllers/other/LoginController.php';
+require_once __DIR__ . '/../controllers/client/ClientController.php';
+require_once __DIR__ . '/../controllers/etablissement/EtablissementController.php';
+require_once __DIR__ . '/../controllers/investisseur/InvestisseurController.php';
+
+//  Étudiant
+Flight::route('GET /etudiants', ['EtudiantController', 'getAll']);
+Flight::route('GET /etudiants/@id', ['EtudiantController', 'getOne']);
+Flight::route('POST /etudiants', ['EtudiantController', 'create']);
+Flight::route('PUT /etudiants/@id', ['EtudiantController', 'update']);
+Flight::route('DELETE /etudiants/@id', ['EtudiantController', 'delete']);
+
+//  Hello
+Flight::route('GET /hello', ['HelloController', 'afficher']);
+
+//  Client 
+Flight::route('GET /client', ['ClientController', 'afficher']);
+Flight::route('GET /create', ['ClientController', 'create']);
+Flight::route('POST /client/create', ['ClientController', 'client_create']);
+
+// Etablissement 
+Flight::route('GET /etablissement', ['EtablissementController', 'afficher']);
+
+//Investisseur
+Flight::route('GET /investisseur', ['InvestisseurController', 'afficher']);
+
+// Login
+Flight::route('GET /login', ['LoginController', 'afficher']);
+Flight::route('POST /login', ['LoginController', 'connecter']);
